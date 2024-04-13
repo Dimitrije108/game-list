ee0d140a8e498831a843782417242cf257f1559d
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2024-04-13 17:46:59 +0200

    feat: add Game class object constructor

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
-console.log('HELLO YOU SEXY EXCITING NEW PROJECT!');
+import html from "./index.html";
+
+class Game {
+    constructor(title, releaseDate, genre, mustPlay, completed) {
+        this._title = title;
+        this._releaseDate = releaseDate;
+        this._genre = genre;
+        this._added = new Date();
+        this._mustPlay = mustPlay;
+        this._completed = completed;
+    }
+
+    set title(value) {
+        this._title = value;
+    }
+
+    set releaseDate(value) {
+        this._releaseDate = value;
+    }
+
+    set genre(value) {
+        this._genre = value;
+    }
+
+    set mustPlay(value) {
+        this._mustPlay = value;
+    }
+
+    set completed(value) {
+        this._completed = value;
+    }
+}
+
+const fallout = new Game('Fallout', '1997', 'RPG', 'Yes', 'No');
+
+console.table(fallout);
+
