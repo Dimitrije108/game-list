49006fa8c102caa986e68d91c0256a1ec530e4af
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2024-04-26 22:37:22 +0200

    feat: add separate module containing utility functions

---
+++ src/utils.js
@@ -0,0 +1,41 @@
+export { createForm, createInput, createDiv, createBtn, appendEl, delEl };
+
+const createForm = () => {
+    const newForm = document.createElement('form');
+    newForm.classList.add('newForm');
+    return newForm;
+};
+
+const createInput = () => {
+    const newInput = document.createElement('input');
+    newInput.setAttribute('type', 'text');
+    newInput.setAttribute('id', 'newLib');
+    newInput.setAttribute('name', 'newLib');
+    newInput.setAttribute('placeholder', 'Name');
+    newInput.setAttribute('minlength', '1');
+    newInput.setAttribute('maxlength', '20');
+    return newInput;
+};
+
+const createDiv = (txtContent, className) => {
+    const newDiv = document.createElement('div');
+    newDiv.textContent = txtContent;
+    newDiv.classList.add(className);
+    return newDiv;
+}
+
+const createBtn = (txtContent, className) => {
+    const newBtn = document.createElement('button');
+    newBtn.textContent = txtContent;
+    newBtn.classList.add(className);
+    return newBtn;
+}
+// Append element
+const appendEl = (parent, child) => {
+    parent.appendChild(child);
+    return parent;
+};
+// Delete element
+const delEl = (el) => {
+    el.remove();
+};
